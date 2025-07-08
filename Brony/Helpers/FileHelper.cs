fadd80bc79d533d944061fd105f8ce4cfd4d30e1
Zokirjon18 <tolqinovzokirjon347@gmail.com>
2025-07-08 16:19:09 +0500

    tayyor

--- Brony/Helpers/FileHelper.cs
+++ Brony/Helpers/FileHelper.cs
@@ -19,25 +19,5 @@ public static class FileHelper
         File.WriteAllLines(filePath, content);
     }
 
-    public static List<string> ToFileFormat<T>(List<T> models)
-    {
-        List<string> modelsInStringFormat = new List<string>();
-
-        foreach (var model in models)
-        {
-            PropertyInfo[] properties = typeof(T).GetProperties();
-            List<string> values = new List<string>();
-
-            foreach (var prop in properties)
-            {
-                object value = prop.GetValue(model);
-                values.Add(value != null ? value.ToString() : "null");
-            }
-
-            string line = string.Join(",", values);
-            modelsInStringFormat.Add(line);
-        }
-
-        return modelsInStringFormat;
-    }
+   
 }
