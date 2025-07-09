966b4a778f49af1cf777426809a6f118780d49f9
hasankhon007 <savriddinovhasanxon007@gmail.com>
2025-07-09 13:31:20 +0500

    Added new model

---
+++ Brony/Domain/Hasanxon.cs
@@ -0,0 +1,18 @@
+﻿namespace Brony.Domain;
+
+public class Hasanxon
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string Occupation { get; set; }
+    public Hasanxon(string name, int age, string occupation)
+    {
+        Name = name;
+        Age = age;
+        Occupation = occupation;
+    }
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Name: {Name}, Age: {Age}, Occupation: {Occupation}");
+    }
+}
